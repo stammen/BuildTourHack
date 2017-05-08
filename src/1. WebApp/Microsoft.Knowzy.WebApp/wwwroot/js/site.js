af7cf8da339a1ec202cadf93561fe6d7f3c7e061
Francisco Manuel Olmedo Bueno <folmedo@plainconcepts.com>
2017-05-08 09:48:19 +0200

    Add jquery for add/remove items

--- src/1. WebApp/Microsoft.Knowzy.WebApp/wwwroot/js/site.js
+++ src/1. WebApp/Microsoft.Knowzy.WebApp/wwwroot/js/site.js
@@ -1,1 +1,28 @@
-﻿// Write your Javascript code.
+﻿$(function () {
+    $('#addItem').on('click', function () {
+        var actualItems = [];
+        $("table").find(".itemNumber").each(function () {
+            actualItems.push($(this).text().trim());
+        });
+
+        $.get("/Shippings/AddOrderItem",
+            $.param({ 'itemNumbers': actualItems }, true), function (partialView) {
+            var tableBody = $('table tbody');
+            tableBody.fadeIn(400,
+                function () {
+                    tableBody.append(partialView);
+                });
+            });
+        (actualItems.length === 3) ? $('#addItem').hide() : $('#addItem').show();
+    });
+
+    $("table").on("click", ".delete", function(event) {
+        event.preventDefault();
+        var tr = $(this).closest('tr');
+        tr.fadeOut(400,
+            function() {
+                tr.remove();
+                $('#addItem').show();
+            });
+    });
+});
