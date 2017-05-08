57d576e7dfb4d3c81f5022629a091f78e97fae4f
Kenny <kreyes@plainconcepts.com>
2017-05-08 17:10:24 +0200

    Add domain classes

---
+++ src/Microsoft.Knowzy.Domain/DevelopmentItem.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Knowzy.Domain.Enums;
+
+namespace Microsoft.Knowzy.Domain
+{
+    public class DevelopmentItem
+    {
+        public string ItemNumber { get; set; }
+        public string Engineer { get; set; }
+        public string NewProductName { get; set; }
+        public string RawMaterial { get; set; }
+        public DevelopmentStatus Status { get; set; }
+    }
+}
