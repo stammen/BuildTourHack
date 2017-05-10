127fd7c8072eaa65d46cdf03e5f0c844df553ccc
Francisco Manuel Olmedo Bueno <folmedo@plainconcepts.com>
2017-05-10 17:26:51 +0200

    Validate quantity in order line

--- src/1. WebApp/Microsoft.Knowzy.WebApp/Startup.cs
+++ src/1. WebApp/Microsoft.Knowzy.WebApp/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
--- src/3. Domain/Microsoft.Knowzy.Models/ViewModels/Validators/OrderLineViewModelValidator.cs
+++ src/3. Domain/Microsoft.Knowzy.Models/ViewModels/Validators/OrderLineViewModelValidator.cs
@@ -7,6 +7,8 @@ namespace Microsoft.Knowzy.Models.ViewModels.Validators
         public OrderLineViewModelValidator()
         {
             RuleFor(orderLine => orderLine.Quantity)
+                .NotEmpty()
+                .WithMessage("A number between 1 - 999 is required")
                 .GreaterThan(0)
                 .WithMessage("At least one item is mandatory")
                 .LessThanOrEqualTo(999)
