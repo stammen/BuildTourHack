b823617272892639a2542dd647ea181f344a0008
Luis Fernández Barba <lfernandez@plainconcepts.com>
2017-05-23 18:17:00 +0000

    Bug fixed
    Bug fixed
    
    Related work items: #31323

--- src/Microsoft.KnowzyJsonDataProvider/Properties/AssemblyInfo.cs
+++ src/Microsoft.KnowzyJsonDataProvider/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.KnowzyJsonDataProvider")]
+[assembly: AssemblyTitle("Microsoft.Knowzy.JsonDataProvider")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Microsoft.KnowzyJsonDataProvider")]
+[assembly: AssemblyProduct("Microsoft.Knowzy.JsonDataProvider")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
