eae5be83087be62677098d2f993256300d2d64a3
stammen <dalestam@microsoft.com>
2017-05-25 14:33:51 -0700

    remove pcl

--- src/Knowzy_Engineering_Win32App/src/Microsoft.KnowzyJsonDataProvider/Properties/AssemblyInfo.cs
+++ src/Knowzy_Engineering_Win32App/src/Microsoft.KnowzyJsonDataProvider/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.Knowzy.JsonDataProvider")]
+[assembly: AssemblyTitle("Microsoft.KnowzyJsonDataProvider")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Microsoft.Knowzy.JsonDataProvider")]
+[assembly: AssemblyProduct("Microsoft.KnowzyJsonDataProvider")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2079a70d-2c88-4a7a-a10b-596c75c8eab9")]
 
 // Version information for an assembly consists of the following four values:
 //
